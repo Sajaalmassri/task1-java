3669e8f9eab898ab672b1754c450a9f799d08d78
saja almassri <sajaalmassri2@gmail.com>
2024-12-26 20:26:31 +0300

    Add files via upload

---
+++ part3.js
@@ -0,0 +1,6 @@
+function addJavaScriptToString(inputString) {
+    return "JavaScript " + inputString;
+}
+const userInput = "is fun to learn!";
+const result = addJavaScriptToString(userInput);
+console.log(result);  
